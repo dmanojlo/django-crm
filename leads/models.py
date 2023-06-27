88cce507f6fe9baba1fe913a66682608d49ac925
Dragan Manojlovic <dragojlo@net.hr>
2023-06-28 00:57:04 +0200

    Ini

--- leads/models.py
+++ leads/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+#from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+
+#User = get_user_model()
+
+#best practice is to create your own abstract user
+class User(AbstractUser):
+    pass
 
 class Agent(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+    
 class Lead(models.Model):
 
     # SOURCE_CHOICES = (
@@ -21,3 +31,5 @@ class Lead(models.Model):
     # profile_picture = models.ImageField(blank=True, null=True)
     # special_files = models.FileField(blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
